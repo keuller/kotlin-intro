5a995439ed2f87086d18f42c5df9931389582bb5
Keuller Magalhaes <keuller.magalhaes@kestraa.com>
2020-11-23 19:43:34 -0300

    Functions.

--- app/src/main/kotlin/com/kotlin/demo/App.kt
+++ app/src/main/kotlin/com/kotlin/demo/App.kt
@@ -1,5 +1,42 @@
 package com.kotlin.demo
 
+// criar tipo de dado
+typealias Handler = (String) -> Unit
+
+//arrow function
+var ola: () -> Unit = {
+    println("Ola Kotliners!")
+}
+
+val greet: Handler = { name ->
+    println("Ola $name!")
+}
+
+// funcao sintaxe lambda
+val greet2 = { nome: String -> 
+    println("Ola $nome")
+}
+
+// funcao com paramentros de entrada e saida
+fun somar(a: Int, b: Int): Int {
+    return a + b
+}
+
+// funcao inline
+fun subtrair(a: Int, b: Int): Int = a - b
+
 fun main() {
     println("Ola Kotlin developers!")
+    ola()
+
+    ola = { println("Ola Kotliners 2!") }
+    ola()
+    
+    val result = somar(5, 5)
+    println("Resultado 5 + 5 = $result")
+
+    val result2 = subtrair(10, 3)
+    println("Resultado 10 - 3 = $result2")
+
+    greet2("Bruno")
 }
