90327f09f8a36e1e72fbd32029f9160ca6359abd
Keuller Magalhaes <keuller.magalhaes@kestraa.com>
2020-11-24 19:55:27 -0300

    Collections.

--- app/src/main/kotlin/com/kotlin/demo/App.kt
+++ app/src/main/kotlin/com/kotlin/demo/App.kt
@@ -35,10 +35,55 @@ fun main() {
         name = "Abdoral Gusmao",
     )
 
-    println("$c1")
+//    println("$c1")
+//
+//    println("Div: 10 / 2 = ${Calculator.execute(com.kotlin.demo.sample.Calculator.DIV, 10, 2)}")
+//    println("Sub: 10 - 3 = ${Calculator.sub(10, 3)}")
+//    println("Mult: 2 * 4 = ${Calculator.mult(2, 4)}")
 
-    println("Div: 10 / 2 = ${Calculator.execute(com.kotlin.demo.sample.Calculator.DIV, 10, 2)}")
-    println("Sub: 10 - 3 = ${Calculator.sub(10, 3)}")
-    println("Mult: 2 * 4 = ${Calculator.mult(2, 4)}")
+//    val nomes: List<String> = listOf("Paulo", "Eduardo", "Giovanna", "Gabriel", "Bruno", "Eduardo")
+//    for(nome in nomes) {
+//        println("Nome: $nome")
+//    }
 
+    val nomes = mutableSetOf("Paulo", "Eduardo", "Giovanna", "Gabriel", "Bruno", "Eduardo")
+    nomes.add("Patricia")
+
+    nomes.filter {
+        nome -> nome.startsWith("P")
+    }.map {
+        "Ola $it"
+    }.forEach {
+        println(it)
+    }
+
+    nomes.clear()
+    println(nomes)
+
+//    val contatos: Set<Contact> = setOf(
+//        Contact("12345", "Abdoral Gusmao", "abdoralgusmao@gmail.com"),
+//        Contact("54321", "Leonard Hofstadter", "leonard@outlook.com"),
+//        Contact("23456", "Sheldon Cooper", "sheldon.cooper@calthec.com"),
+//        Contact("65432", "Penny", "penny@gmail.com"),
+//        Contact("12345", "Abdoral Gusmao", "abdoralgusmao@gmail.com")
+//    )
+//
+//    contatos.forEach { println("Personagem: $it") }
+
+
+    val cidades = mutableMapOf(
+        "SP" to "Sao Paulo",
+        "SJC" to "Sao Jose dos Campos",
+        "SA" to "Santo Andre",
+        "SBC" to "Sao Bernardo do Campo"
+    )
+
+    cidades["RJ"] = "Rio de Janeiro"
+
+//    for ((key, value) in cidades) {
+//        println("$key => $value")
+//    }
+
+    cidades.keys.forEach { println("$it => ${cidades[it]} ")}
+    cidades.values.forEach { println("Cidade: $it") }
 }
